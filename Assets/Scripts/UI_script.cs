5f7c84695702d41b2139e960c8db5003c4b744bf
rayjk14 <rayjk14@gmail.com>
2021-10-17 18:13:47 -0400

    Added UI
    Added UI and Intro Video scenes and associated assets.

---
+++ Assets/Scripts/UI_script.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_script : MonoBehaviour
+{
+    GameObject mainMenu, tireMenu, jumpMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu = GameObject.Find("Main Menu");
+        tireMenu = GameObject.Find("Tire Change Submenu");
+        jumpMenu = GameObject.Find("Battery Jump Submenu");
+
+        mainMenu.SetActive(true);
+        tireMenu.SetActive(false);
+        jumpMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void tireChangePressed()
+    {
+        mainMenu.SetActive(false);
+        tireMenu.SetActive(true);
+    }
+
+    public void tireChangeBack()
+    {
+        tireMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void batteryJumpPressed()
+    {
+        mainMenu.SetActive(false);
+        jumpMenu.SetActive(true);
+    }
+
+    public void batteryJumpBack()
+    {
+        jumpMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void exitToDesktop()
+    {
+        Application.Quit();
+        Debug.Log("Exiting to Desktop");
+    }
+
+}
