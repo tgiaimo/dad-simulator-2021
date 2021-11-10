24e1cab4b528c4ddd5f523bdfb253b92a716d1c8
Judsen <jhembre@g.clemson.edu>
2021-11-09 19:32:00 -0500

    little stuff

---
+++ Assets/deadBatPosTerminal.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class deadBatPosTerminal : MonoBehaviour
+{
+    public GameObject expectedClip1;
+    public GameObject expectedClip2;
+    public GameObject clipped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if((other.gameObject.name == expectedClip1.name) || (other.gameObject.name == expectedClip2.name))
+        {
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            clipped.GetComponent<clipCheck>().clipped = true;
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if((other.gameObject.name == expectedClip1.name)||(other.gameObject.name == expectedClip2.name))
+        {
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            clipped.GetComponent<clipCheck>().clipped = false;
+        }
+    }
+
+}
